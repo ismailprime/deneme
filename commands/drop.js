d48b850677f01d48868c5ac78e7f3b53b455031f
ismailprime <isseat@hotmail.com>
2026-06-16 19:08:49 +0300

    Update drop.js

--- commands/drop.js
+++ commands/drop.js
@@ -1,1 +1,18 @@
+module.exports = {
+  name: "drop",
 
+  async execute(message, client, args) {
+    const prize = args.join(" ");
+    if (!prize) return message.reply("Ödül yaz!");
+
+    const msg = await message.channel.send(`⚡ DROP! İlk tıklayan kazanır: **${prize}**`);
+
+    const filter = i => !i.user.bot;
+
+    const collector = msg.channel.createMessageComponentCollector({ filter, max: 1 });
+
+    collector.on("collect", i => {
+      msg.edit(`🏆 ${i.user} ödülü kazandı! Ticket açarak alabilirsin.`);
+    });
+  }
+};
