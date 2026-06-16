681ff2aaef8eeded59e54d4b09b41747505c22c1
ismailprime <isseat@hotmail.com>
2026-06-16 19:07:46 +0300

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -1,1 +1,15 @@
+module.exports = {
+  name: "ip",
 
+  execute(message) {
+    message.reply(`
+**Java**
+Sürüm: 1.9 - 1.21.x
+Bağlantı Adresi: mc.skyforgenw.com.tr
+
+**Bedrock**
+Sürüm: Yakında
+Bağlantı Adresi: mc.skyforgenw.com.tr
+`);
+  }
+};
