29464e5fe323a064a3d915293cb059abc54b230e
ismailprime <isseat@hotmail.com>
2026-06-16 19:21:49 +0300

    Update commandHandler.js

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,1 +1,10 @@
+const fs = require("fs");
 
+module.exports = (client) => {
+  const files = fs.readdirSync("./commands");
+
+  for (const file of files) {
+    const cmd = require(`../commands/${file}`);
+    client.commands.set(cmd.name, cmd);
+  }
+};
