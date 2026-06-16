848f850b302f6877c6761a48e8c34524eafff929
ismailprime <isseat@hotmail.com>
2026-06-16 20:01:13 +0300

    Update cekilis.js

--- commands/cekilis.js
+++ commands/cekilis.js
@@ -1,17 +1,40 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const ms = require("ms");
 
 module.exports = {
   name: "cekilis",
 
-  execute(message, client, args) {
+  async execute(message, client, args) {
+
     const time = ms(args[0]);
     const prize = args.slice(1).join(" ");
 
-    message.channel.send(`🎉 ÇEKİLİŞ: ${prize}`);
+    const participants = [];
 
-    setTimeout(() => {
-      const winner = message.guild.members.cache.random();
-      message.channel.send(`🏆 Kazanan: <@${winner.id}>`);
-    }, time);
+    const button = new ButtonBuilder()
+      .setCustomId("join")
+      .setLabel("Katıl")
+      .setStyle(ButtonStyle.Success);
+
+    const row = new ActionRowBuilder().addComponents(button);
+
+    const msg = await message.channel.send({
+      content: `🎉 ${prize}`,
+      components: [row]
+    });
+
+    const collector = msg.createMessageComponentCollector({ time });
+
+    collector.on("collect", i => {
+      if (!participants.includes(i.user.id)) {
+        participants.push(i.user.id);
+        i.reply({ content: "Katıldın", ephemeral: true });
+      }
+    });
+
+    collector.on("end", () => {
+      const winner = participants[Math.floor(Math.random() * participants.length)];
+      msg.edit(`🏆 Kazanan: <@${winner}>`);
+    });
   }
 };
