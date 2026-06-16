7bb3eefffff9cc2ec970fe50799886f109d426c3
ismailprime <isseat@hotmail.com>
2026-06-16 19:17:17 +0300

    Update cekilis.js

--- commands/cekilis.js
+++ commands/cekilis.js
@@ -4,18 +4,14 @@ module.exports = {
   name: "cekilis",
 
   async execute(message, client, args) {
-    if (!args[0]) return message.reply("Süre yaz!");
-
     const time = ms(args[0]);
     const prize = args.slice(1).join(" ");
 
-    if (!prize) return message.reply("Ödül yaz!");
-
-    const msg = await message.channel.send(`🎉 ÇEKİLİŞ BAŞLADI! Ödül: **${prize}**`);
+    const msg = await message.channel.send(`🎉 ÇEKİLİŞ: **${prize}**`);
 
     setTimeout(() => {
       const winner = message.guild.members.cache.random();
-      msg.reply(`🏆 Kazanan: <@${winner.id}>`);
+      message.channel.send(`🏆 Kazanan: <@${winner.id}> 🎉`);
     }, time);
   }
 };
