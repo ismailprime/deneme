7a7c6e204772ff2f4911f9aa466d468c6921ba31
ismailprime <isseat@hotmail.com>
2026-06-16 19:08:11 +0300

    Update bilgi.js

--- commands/bilgi.js
+++ commands/bilgi.js
@@ -1,1 +1,14 @@
+module.exports = {
+  name: "bilgi",
 
+  execute(message, client) {
+
+    message.reply(`
+📊 Sunucu Bilgisi
+
+👥 Üye: ${message.guild.memberCount}
+🏓 Ping: ${client.ws.ping}ms
+🌐 IP: mc.skyforgenw.com.tr
+`);
+  }
+};
