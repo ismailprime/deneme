530d7c40fc686ddd78bd235286d8998602acab26
ismailprime <isseat@hotmail.com>
2026-06-16 20:30:06 +0300

    Update cekilis.js

--- commands/cekilis.js
+++ commands/cekilis.js
@@ -8,39 +8,39 @@ module.exports = {
     const time = args[0];
     const prize = args.slice(1).join(" ");
 
-    if (!time || !prize) {
-      return message.reply("!cekilis 1m ödül");
-    }
+    let ms = 60000;
+    if (time?.endsWith("m")) ms = parseInt(time) * 60000;
+    if (time?.endsWith("h")) ms = parseInt(time) * 3600000;
 
-    const participants = [];
+    const users = [];
 
-    const button = new ButtonBuilder()
-      .setCustomId("join_giveaway")
-      .setLabel("Katıl")
-      .setStyle(ButtonStyle.Success);
-
-    const row = new ActionRowBuilder().addComponents(button);
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("giveaway_join")
+        .setLabel("Katıl")
+        .setStyle(ButtonStyle.Success)
+    );
 
     const msg = await message.channel.send({
       content: `🎉 ${prize}`,
       components: [row]
     });
 
-    const collector = msg.createMessageComponentCollector({ time: 60000 });
+    const collector = msg.createMessageComponentCollector({ time: ms });
 
     collector.on("collect", (i) => {
-      if (!participants.includes(i.user.id)) {
-        participants.push(i.user.id);
+      if (!users.includes(i.user.id)) {
+        users.push(i.user.id);
         i.reply({ content: "Katıldın", ephemeral: true });
       }
     });
 
     collector.on("end", () => {
 
-      if (participants.length === 0)
+      if (users.length === 0)
         return msg.edit("Katılım yok");
 
-      const winner = participants[Math.floor(Math.random() * participants.length)];
+      const winner = users[Math.floor(Math.random() * users.length)];
 
       msg.edit(`🏆 Kazanan: <@${winner}>`);
     });
