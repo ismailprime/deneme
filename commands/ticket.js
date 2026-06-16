2ca3a6426bd5d89bdf6fd594aaaf8664da378b6c
ismailprime <isseat@hotmail.com>
2026-06-16 19:09:11 +0300

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,1 +1,12 @@
+module.exports = {
+  name: "ticket",
 
+  async execute(message) {
+    const channel = await message.guild.channels.create({
+      name: `ticket-${message.author.username}`,
+      type: 0
+    });
+
+    channel.send(`🎟 Ticket açıldı <@${message.author.id}>`);
+  }
+};
