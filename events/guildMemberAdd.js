08c583d3e8c68c0f3d687b85c91a34a7c6f4c5cd
ismailprime <isseat@hotmail.com>
2026-06-16 19:07:18 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,15 @@
+const config = require("../config/config");
 
+module.exports = {
+  name: "guildMemberAdd",
+
+  async execute(member) {
+    const channel = member.guild.systemChannel;
+    if (!channel) return;
+
+    channel.send(`👋 <@${member.id}> aramıza hoşgeldin! 🎉`);
+
+    const role = member.guild.roles.cache.get(config.autoRole);
+    if (role) member.roles.add(role);
+  }
+};
