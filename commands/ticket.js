9a010a791d4b17aefbb421e9bf2f68a1104c12c7
ismailprime <isseat@hotmail.com>
2026-06-16 19:33:44 +0300

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,16 +1,22 @@
-const { ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js");
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
   name: "ticket",
 
   execute(message) {
-    const btn = new ButtonBuilder()
-      .setCustomId("ticket_open")
-      .setLabel("Ticket Aç")
-      .setStyle(ButtonStyle.Primary);
 
-    const row = new ActionRowBuilder().addComponents(btn);
+    const embed = new EmbedBuilder()
+      .setTitle("🎟 Destek Sistemi")
+      .setDescription("Ticket açmak için butona bas")
+      .setColor("Blue");
 
-    message.channel.send({ content: "🎟 Ticket panel", components: [row] });
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("ticket_open")
+        .setLabel("Ticket Aç")
+        .setStyle(ButtonStyle.Primary)
+    );
+
+    message.channel.send({ embeds: [embed], components: [row] });
   }
 };
